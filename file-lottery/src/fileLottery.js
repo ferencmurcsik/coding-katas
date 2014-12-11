99f56e1a5e6cffef2caf2983c53125bd1dea7bf1
ferencmurcsik <ferenc.murcsik@gmail.com>
2014-12-11 10:23:09 +0100

    Update fileLottery.js
    test

--- file-lottery/src/fileLottery.js
+++ file-lottery/src/fileLottery.js
@@ -1,33 +1,25 @@
-
-var fileLottery = function( directoryPath ) {
-	this.index = -1;
-	this.elements = fileLottery.readFiles(directoryPath);
+var fileLottery = function(directory) {
+  	this.index = 0;
+  	this.fileArray = ["fiel1", "file2", "file3"];
+   // return directory;
+   
 }
-
-fileLottery.prototype = {
-	next: function() {
-		this.index++;
-		return this.elements[this.index];
-	},
-	shuffle: function() {
-		var shuffled = [];
-		for (var i=0; i <= this.elements.length; i++) {
-			var randomIndex = fileLottery.generateRandom(0,this.elements.length-1);
-			shuffled[i] = this.elements.splice(randomIndex,1)[0];
+	fileLottery.prototype = {
+		next : function(){
+			return this.fileArray[this.index++];
+		},
+		randomInRange : function(min, max) {
+		    return Math.floor(Math.random() * (max - min + 1)) + min;
+		},
+		shuffle : function(){
+		    var shuffled = [];
+		    for (i = 0; i <= this.fileArray.length; i++) {
+		        var randomIndex = this.randomInRange(0,this.fileArray.length-1);
+		        console.log(this.fileArray);
+			    shuffled[i] = this.fileArray.splice(randomIndex,1)[0];
+		    }
+			return shuffled;
 		}
-		this.elements = shuffled;
-		return this.elements;
 	}
-}
-
-fileLottery.generateRandom = function( min, max ) {
-		return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-fileLottery.readFiles = function(directoryPath) {
-		var fs = require("fs");
-	    return fs.readdirSync(directoryPath);
-}
 
 module.exports.fileLottery = fileLottery;
-
