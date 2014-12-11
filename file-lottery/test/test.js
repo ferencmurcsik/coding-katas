a2cacc6ef4bf8ebfeb08fe7e1e89bd5afa48d163
ferencmurcsik <ferenc.murcsik@gmail.com>
2014-12-11 10:23:43 +0100

    Update test.js

--- file-lottery/test/test.js
+++ file-lottery/test/test.js
@@ -6,66 +6,34 @@ var path = require('path');
 
 var fileLottery = require('../src/fileLottery.js').fileLottery;
 
-var TEST_FILE_LIST = [ 'file1', 'file2', 'file3' ];
+suite('Returns funcion', function() {
 
-suite('fileLottery', function() {
-
-  var mockReadFiles = sinon.stub(fileLottery, "readFiles");
-  mockReadFiles.withArgs("testdir").returns(TEST_FILE_LIST);
-  var f = new fileLottery("testdir"); 
-  
-  test ('It returns a file from a directory', function() {
-    assert.equal("file1",f.next());
-  });
-
-  test ('It returns a separate file for the second call', function() {
-    assert.equal("file2",f.next());
-  });
-
-  test ('We shuffle the file list in the directory', function() {
-    var mockRandomGenerator = sinon.stub(fileLottery,"generateRandom");
-    mockRandomGenerator.onFirstCall().returns(1);
-    mockRandomGenerator.onSecondCall().returns(0);
-    assert.deepEqual( ["file2", "file1"], f.shuffle() );
-    fileLottery.generateRandom.restore();
-  });
-  
-  fileLottery.readFiles.restore();
-  
-});
-
-suite('Random functions', function() {
-
-  test ('We generate a random number in a range', function() {
-    assert( fileLottery.generateRandom(0,10) <= 10 );
-    assert( fileLottery.generateRandom(0,10) >= 0 );
+  var fileLot = new fileLottery("");
+  test ('return the file', function() {
+    assert.equal("fiel1", fileLot.next());
   }); 
-
-});
-
-
-suite('fileLottery readFiles', function() {
- 
-  test ('read files from a given directory path', function() {
-    var path = (__dirname + "/../src");
-    assert.deepEqual( ["fileLottery.js"], fileLottery.readFiles(path) );
-  });
   
+  test('retunt two files', function() {
+     assert.equal("file2", fileLot.next()); 
+     expect("meresz").contain.to.contain("esz");
+  });
+//   test ('returns ', function() {
+//     assert.equal( , );
+  //}); 
+
 });
 
-suite('fileLottery output', function() {
- 
-  test ('print results in terminal', function() {
-    var path = (__dirname + "/../node_modules/mocha");
-    var f = new fileLottery(path);
-    f.shuffle();
-    console.log(f.next());
-    console.log(f.next());
-    console.log(f.next());
-    console.log(f.next());
-    console.log(f.next());
-    console.log(f.next());
-    console.log(f.next());
-  });
-  
+suite('Random function', function() {
+   var fileLot = new fileLottery("dir");
+   test('return in a range', function() {
+      assert(fileLot.randomInRange(1,6) <= 6);
+      assert(fileLot.randomInRange(1,6) >= 1);
+   });
 });
+
+suite('file random returns', function() {
+  var fileLot = new fileLottery("dir");
+  test('shuffle works correctly',function(){
+     assert.deepEqual(["file2","fiel1"], fileLot.shuffle());
+  });
+})
